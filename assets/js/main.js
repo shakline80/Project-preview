74c33549c631cad4ef582cbe6333a8e1b1474f90
Shakline <55685810+shakline80@users.noreply.github.com>
2025-05-15 00:59:14 +0600

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,3 +19,24 @@
 			passwordIcon.src = 'assets/img/blind-eye.svg'; // back to view icon
 		}
 	}
+
+
+
+
+
+	const reveals = document.querySelectorAll(".reveal");
+
+  function revealOnScroll() {
+    for (let i = 0; i < reveals.length; i++) {
+      const windowHeight = window.innerHeight;
+      const elementTop = reveals[i].getBoundingClientRect().top;
+      const revealPoint = 100;
+
+      if (elementTop < windowHeight - revealPoint) {
+        reveals[i].classList.add("active");
+      }
+    }
+  }
+
+  window.addEventListener("scroll", revealOnScroll);
+  window.addEventListener("load", revealOnScroll); // For initial view
